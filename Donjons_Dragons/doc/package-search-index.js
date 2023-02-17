e568a5286a137c47df0d583f2564a1ed352c145d
julienouguier3 <nouguier.j3@gmail.com>
2023-02-17 12:09:37 +0100

    init javadoc

---
+++ Donjons_Dragons/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"defensiveEquipment"},{"l":"equipment"},{"l":"exceptions"},{"l":"gameboard"},{"l":"offensiveEquipment"},{"l":"personnages"},{"l":"Test"}];updateSearchResults();
